6ca7cebfc9e01ba80e2319a9e48850322f2b2b4d
Davi Banfi <davibanfi1579@outlook.com>
2022-04-25 14:52:33 -0300

    feat: ensure users can mint coins

--- contracts/Ducats.sol
+++ contracts/Ducats.sol
@@ -3,22 +3,49 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Ducats is ERC20, Ownable {
-    mapping (address => uint) ownerToDucats;
-    uint fee;
+    uint256 fee;
+    uint256 private maximumSupply;
+    uint private ducatsFromContract;
 
-    constructor(uint256 initialSupply) ERC20("Ducats", "DUC") {
-        _mint(msg.sender, initialSupply);
+    using SafeMath for uint256;
+
+    constructor(uint256 maximum) ERC20("Ducats", "DUC") {
+        maximumSupply = maximum;
         fee = 10;
     }
 
-    function changeFee(uint amount) public onlyOwner {
+    function buy(uint256 amount) public {
+        require(totalSupply().add(amount) <= maximumSupply, "Contract reached its maximum supply, cannot mint more coins.");
+        _mint(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns(bool) {
+        uint tax = amount.div(fee);
+        require(balanceOf(msg.sender) >= amount.add(tax), "You don't have enought money to pay the fee.");
+
+        _transfer(msg.sender, to, amount);
+        ducatsFromContract = ducatsFromContract.add(tax);
+
+        return true;
+    }
+
+    function setFee(uint amount) public onlyOwner {
         require(amount >= 0 && amount <= 100, "The fee must be a percentage.");
         fee = amount;
     }
 
     function getFee() public view returns(uint) {
         return fee;
+    }
+
+    function setMaximum(uint amount) public onlyOwner {
+        maximumSupply = amount;
+    }
+
+    function getMaximum() public view returns(uint) {
+        return maximumSupply;
     }
 }
--- test/Ducats.test.js
+++ test/Ducats.test.js
@@ -10,12 +10,12 @@ const assert = chai.assert
 require("dotenv").config({path: "../.env"})
 
 contract("Ducats Test", async (accounts) => {
-    const [deployerAccount, recipient] = accounts
+    const [deployerAccount, account1] = accounts
 
     let ducats
 
     beforeEach(async () => {
-        ducats = await Ducats.new(process.env.INITIAL_DUCATS || 1000000)
+        ducats = await Ducats.new(process.env.INITIAL_DUCATS)
     })
     
     it("should instantiate smart contract with initial fee of 10%.", async () => {
@@ -24,37 +24,39 @@ contract("Ducats Test", async (accounts) => {
         return assert.equal((await instance.getFee()).toString(), new BN(10))
     })
 
-    it("should be possible to change fee, if onwer wants to.", async () => {
+    it("should be possible to set fee, if onwer wants to.", async () => {
         const instance = ducats
 
-        await instance.changeFee(20)
+        await instance.setFee(20)
 
         return assert.equal((await instance.getFee()).toString(), new BN(20))
     })
 
-    it("should be possible to send Ducats between accounts.", async () => {
-        const sendDucats = 5;
-
+    it("should not be possible to set fee higher than 100 or lower than 0.", async () => {
         const instance = ducats
 
-        const totalSupply = await instance.totalSupply()
+        expect(instance.setFee(-1)).to.eventually.be.rejected
 
-        assert.equal((await instance.balanceOf(deployerAccount)).toString(), totalSupply.toString())
+        expect(instance.setFee(101)).to.eventually.be.rejected
 
-        await instance.transfer(recipient, sendDucats)
-
-        assert.equal((await instance.balanceOf(deployerAccount)).toString(), totalSupply.sub(new BN(sendDucats)).toString())
-
-        return assert.equal((await instance.balanceOf(recipient)).toString(), new BN(sendDucats).toString())
+        assert.equal((await instance.getFee()).toString(), new BN(10))
     })
 
-    it("should not be possible to send more tokens than available in total.", async () => {
+    it("should be possible to buy ducats.", async () => {
+        const ducatsToBuy = 10
+
         const instance = ducats
 
-        const balanceOfDeployer = await instance.balanceOf(deployerAccount)
+        await instance.buy(ducatsToBuy)
 
-        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected
+        assert.equal((await instance.balanceOf(deployerAccount)).toString(), ducatsToBuy);
+    })
 
-        return assert.equal((await instance.balanceOf(deployerAccount)).toString(), balanceOfDeployer.toString())
+    it("should not be possible to buy ducats if maximum supply was reached.", async () => {
+        const instance = ducats
+
+        const ducatsToBuy = new BN(await instance.getMaximum() + 1)
+
+        return expect(instance.buy(ducatsToBuy + 1)).to.eventually.be.rejected
     })
 })
