ce99b550b2eb62641369f16a185f2bde6098cfe2
Davi Banfi <davibanfi1579@outlook.com>
2022-04-27 12:38:30 -0300

    chore: change config to connect with ganache

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,4 @@
 const path = require("path");
-require("dotenv").config({path: "./.env"});
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -13,24 +10,6 @@ module.exports = {
       host: "localhost",
       network_id: 5777
     }
-    // ganache_local: {
-    //   provider: function() {
-    //     return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545", AccountIndex)
-    //   },
-    //   network_id: 5777
-    // },
-    // goerli_infura: {
-    //   provider: function() {
-    //     return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/ffa60321cbbd4cfda3352014c556e3c3", AccountIndex)
-    //   },
-    //   network_id: 5
-    // },
-    // ropsten_infura: {
-    //   provider: function() {
-    //     return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/ffa60321cbbd4cfda3352014c556e3c3", AccountIndex)
-    //   },
-    //   network_id: 3
-    // }
   },
   compilers: {
     solc: {
